5fba5e64d65de468025ffe2f0e6d92eb918e50d2
dvirbestt <dvirbest1122@gmail.com>
2022-11-22 22:26:39 +0200

    added a navbar Prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Navbar from './Components/Navbar'
 
 function App() {
   
 
   return (
     <div className="App">
-    
+      <Navbar/>
     </div>
   )
 }
